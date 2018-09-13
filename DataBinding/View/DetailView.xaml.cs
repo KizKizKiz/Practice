2769452399856e1aba71ba7f95bfb3f7e1ea748d
Max <h.onto@yandex.ru>
2018-09-13 14:17:10 +0300

    added two window, commands

---
+++ DataBinding/View/DetailView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Windows;
+using DataBinding.ViewModel;
+namespace DataBinding.View
+{
+    /// <summary>
+    /// Логика взаимодействия для DetailView.xaml
+    /// </summary>
+    public partial class DetailView : Window
+    {
+        public DetailView()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
---
+++ DataBinding/ViewModel/ZooViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using System.Configuration;
+using Task_1.Core;
+using Task_1;
+using DataBinding.View;
+
+namespace DataBinding.ViewModel
+{
+    class ZooViewModel : ViewModelBase
+    {
+        private Essential _essential;
+        private RelayCommand _detail;
+        /// <summary>
+        /// Отображает окно с детальной информацией о животном
+        /// </summary>
+        public RelayCommand Detail
+        {
+            get
+            {
+                return _detail ??
+                    (_detail = new RelayCommand(
+                    (obj) => new AnimalDetailViewModel(SelectedAnimal, _essential),
+                    (obj) => obj != null));
+            }
+        }
+        private Animal _selectedAnimal;
+        /// <summary>
+        /// Получает/задает выбранное животное
+        /// </summary>
+        public Animal SelectedAnimal
+        {
+            get
+            {
+                return _selectedAnimal;
+            }
+            set
+            {
+                SetProperty<Animal>(ref _selectedAnimal, value);
+            }
+        }
+
+        private List<Animal> _animals;
+        /// <summary>
+        /// Получает/задает коллекцию животных 
+        /// </summary>
+        public List<Animal> Animals
+        {
+            get
+            {
+                return _animals;
+            }
+            set
+            {
+                SetProperty(ref _animals, value);
+            }
+        }        
+        public ZooViewModel()
+        {
+            _essential = new Essential();
+            _essential.ConnectionString = ConfigurationSettings.AppSettings["AnimalSqlProvider"];
+            Animals = _essential.Load("SELECT * FROM ANIMALS");
+        }
+    }
+}
