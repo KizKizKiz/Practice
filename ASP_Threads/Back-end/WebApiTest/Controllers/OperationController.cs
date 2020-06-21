dd4995cf2c2b95a9a8c0eec8071cd00b0e5bdfd8
Max <h.onto@yandex.ru>
2020-06-21 19:19:31 +0300

    ASP_Threads - implement webApi back-end and WPF front-end applications

---
+++ ASP_Threads/Back-end/WebApiTest/Controllers/OperationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApiTest.Controllers
+{
+
+    [Route("api")]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet("operation")]
+        public async Task<int> LongOperation()
+        {
+            //Simulate long operation
+            await Task.Delay(3000);
+
+            var rnd = new Random();
+            return rnd.Next(1000);
+        }
+    }
+}
